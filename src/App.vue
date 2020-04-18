4b25f350c12d3f8d0601f67b470e8e6944185d40
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-04-18 15:45:22 +0200

    fix: body margin, router animations

--- src/App.vue
+++ src/App.vue
@@ -64,9 +64,13 @@ export default {
 .fade-enter-active,
 .fade-leave-active {
   transition-duration: 0.3s;
-  transition-property: opacity;
   transition-property: height, opacity;
   transition-timing-function: ease;
   overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
